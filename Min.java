ab2a78b43fde4d1ca2599b2f50c36e38a3d77b52
lassaadbaati <lassaad.baati@gmail.com>
2021-06-27 00:40:21 +0100

    Creation fichier Main.java
    commit 1 : creation du fichier Main.java

---
+++ Min.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      System.out.println(" ######### DEBUT #############");
+      System.out.println(" Hi De la part de l uit !");
+      System.out.println(" ######### FIN #############");
+    }
+}
