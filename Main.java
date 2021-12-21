985098c1dcab509ac20f1577a34ff952e0a8a24d
lassaadbaati <lassaad.baati@gmail.com>
2021-12-21 10:37:03 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println(" ######### DEBUT #############");
-      System.out.println(" Hi De la part de l_ISG !");
+      System.out.println(" Bonnes vacances !");
       System.out.println(" ######### FIN #############");
     }
 }
