e5127f910aa5f1992de1f1b821db62402372b98b
lassaad Baati <lassaad.baati@gmail.com>
2022-04-01 10:21:23 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println(" ######### DEBUT #############");
-      System.out.println(" Bonnes vacances !");
+      System.out.println(" Formation DEVOPS !");
       System.out.println(" ######### FIN #############");
     }
 }
