fea20049d63cce23260962c6bb14d5b9e5c12225
lassaad Baati <lassaad.baati@gmail.com>
2022-11-15 16:06:09 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
       System.out.println(" ######### DEBUT #############");
-      System.out.println(" Formation DEVOPS ! Nouvelle modification");
+      System.out.println(" Formation DEVOPS ! Nouvelle modification du 15 11 2022");
       System.out.println(" ######### FIN #############");
     }
 }
